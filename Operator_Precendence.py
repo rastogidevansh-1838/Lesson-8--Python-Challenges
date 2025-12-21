04475161fac7eb41d618fa78de35f0caa2efc78d
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-12-21 10:15:41 +0530

    first commit

---
+++ Operator_Precendence.py
@@ -0,0 +1,13 @@
+a = 4
+f = 3
+v = 2
+x = 9
+h = 0
+h = (a+f) * x / v
+print("Value of (a+f) * v/ x is ", h)
+name= "Alex"
+age = 0
+if name == "Alex" or name == "John" and age >= 2 :
+ print ("Hello and Welcome")
+else:
+ print("Good Bye!!")
